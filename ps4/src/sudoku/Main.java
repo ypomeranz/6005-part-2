0f5a17bb4452765ad721a6f11b252025356661e7
yoni <yoni.pomeranz@hotmail.com>
2016-08-21 21:18:34 +0000

    main seems to work fine with no further editing - is i possible that i wrote this all right the first time?

--- ps4/src/sudoku/Main.java
+++ ps4/src/sudoku/Main.java
@@ -15,14 +15,14 @@ public class Main {
      */
     public static void main (String[] args) {
         timedSolve (new Sudoku(2));
-//        timedSolve (new Sudoku(2, new int[][] { 
-//                    new int[] { 0, 1, 0, 4 }, 
-//                    new int[] { 0, 0, 0, 0 }, 
-//                    new int[] { 2, 0, 3, 0 }, 
-//                    new int[] { 0, 0, 0, 0 }, 
-//        }));
-//        timedSolveFromFile(3, "samples/sudoku_easy.txt");
-//        timedSolveFromFile(3, "samples/sudoku_hard.txt");        
+        timedSolve (new Sudoku(2, new int[][] { 
+                    new int[] { 0, 1, 0, 4 }, 
+                    new int[] { 0, 0, 0, 0 }, 
+                    new int[] { 2, 0, 3, 0 }, 
+                    new int[] { 0, 0, 0, 0 }, 
+        }));
+        timedSolveFromFile(3, "samples/sudoku_easy.txt");
+        timedSolveFromFile(3, "samples/sudoku_hard.txt");        
     }
 
     /**
